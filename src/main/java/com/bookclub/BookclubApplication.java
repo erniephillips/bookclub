f0315497f33834d2a9820ac8b0eada32f7ff5628
Ernie Phillips <eraphillips@my365.bellevue.edu>
2021-09-27 20:58:24 -0400

    adding encryption

--- src/main/java/com/bookclub/BookclubApplication.java
+++ src/main/java/com/bookclub/BookclubApplication.java
@@ -1,17 +1,22 @@
 /*
     Krasso, R., (2021). CIS 505 Intermediate Java Programming. Bellevue University, all  rights reserved.
     URL: https://github.com/buwebdev/cis-530
+         https://www.geeksforgeeks.org/how-to-encrypt-passwords-in-a-spring-boot-project-using-jasypt/
     Modified By: Ernie Phillips III
     Created Date: 09/01/2021
     Modified Date: 09/27/2021
     Purpose: Main class for bootstrapping the application
+    Modification: Adding Jasypt annotation to tell app to look for decrypt key and ENC() values anywhere in app
 */
 package com.bookclub;
+
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication 
+@EnableEncryptableProperties //Adding Jasypt annotation to tell app to look for decrypt key and ENC() values anywhere in app
 public class BookclubApplication {
 
     public static void main(String[] args) {
